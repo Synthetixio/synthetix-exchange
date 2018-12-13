89ec26ac21f57f5e38e3a14f7f90e8505d8f45da
Clement Balestrat <>
2018-12-13 12:13:34 +1100

    input handler OK

--- src/components/trading-widget/trading-widget.js
+++ src/components/trading-widget/trading-widget.js
@@ -13,16 +13,39 @@ import {
 
 import styles from './trading-widget.module.scss';
 
-class SynthTransaction extends Component {
+class TradingWidget extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValues: {},
+    };
+  }
+
+  onInputChange(synth) {
+    return e => {
+      const { inputValues } = this.state;
+      const currentInputValue = inputValues[synth] || 0;
+      const newInputValue = e.target.validity.valid
+        ? e.target.value
+        : currentInputValue;
+      this.setState({
+        inputValues: { ...inputValues, [synth]: newInputValue },
+      });
+    };
+  }
+
   renderInputs() {
     const { synthToBuy, synthToExchange } = this.props;
+    const { inputValues } = this.state;
     const inputs = [synthToExchange, synthToBuy].map(synth => {
       return (
         <div className={styles.widgetInputWrapper}>
           <input
             className={styles.widgetInputElement}
             type="text"
-            value={'0.00'}
+            value={inputValues[synth]}
+            onChange={this.onInputChange(synth)}
+            pattern="^-?[0-9]\d*\.?\d*$"
           />
           <div className={styles.widgetInputSynth}>
             <img src={`images/synths/${synth}-icon.svg`} alt="synth icon" />
@@ -57,7 +80,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {};
 
-SynthTransaction.propTypes = {
+TradingWidget.propTypes = {
   currentWalletInfo: PropTypes.object.isRequired,
   synthToBuy: PropTypes.string.isRequired,
   synthToExchange: PropTypes.string.isRequired,
@@ -66,4 +89,4 @@ SynthTransaction.propTypes = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SynthTransaction);
+)(TradingWidget);
