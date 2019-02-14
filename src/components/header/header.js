8584fcb37b2a39f39f0ee2f476d9e073d412d765
Clement Balestrat <clement.balestrat@gmail.com>
2019-02-15 08:19:56 +1100

    header change

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getCurrentWalletInfo } from '../../ducks/';
+
 import WalletAddressSwitcher from '../wallet-address-switcher';
 import styles from './header.module.scss';
 
-const Header = () => {
-  return (
-    <div className={styles.header}>
-      <div className={styles.logoWrapper}>
-        <img height="36" alt="logo" src="/images/synthetix-logo.svg" />
-        <span className={styles.beta}>[beta]</span>
+class Header extends Component {
+  constructor() {
+    super();
+  }
+
+  onEnvButtonClick() {
+    console.log('yo');
+  }
+
+  render() {
+    return (
+      <div className={styles.header}>
+        <div className={styles.logoWrapper}>
+          <img height="36" alt="logo" src="/images/synthetix-logo.svg" />
+          <span className={styles.beta}>[beta]</span>
+          <button
+            type="button"
+            onClick={this.onEnvButtonClick}
+            className={styles.envButton}
+          >
+            Mainnet
+          </button>
+        </div>
+        <WalletAddressSwitcher />
       </div>
-      <WalletAddressSwitcher />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    currentWalletInfo: getCurrentWalletInfo(state),
+  };
 };
 
-export default Header;
+const mapDispatchToProps = {};
+
+Header.propTypes = {
+  currentWalletInfo: PropTypes.object.isRequired,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
