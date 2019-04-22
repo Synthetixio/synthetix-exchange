4dc9dd5a80625f9d69e9d40dd8048e0f300ec14e
justin j. moses <justinjmoses@gmail.com>
2019-04-22 18:12:59 -0400

    Minor cleanup to fix linter

--- scripts/build.js
+++ scripts/build.js
@@ -14,7 +14,6 @@ process.on('unhandledRejection', err => {
 // Ensure environment variables are read.
 require('../config/env');
 
-
 const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
