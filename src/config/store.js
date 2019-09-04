aec426579180768dd4af44f39be38dd6b74da4d7
Sergey Vassilyev <sergey@synthetix.io>
2019-09-05 00:07:02 +1000

    persisting exchangemode

--- src/config/store.js
+++ src/config/store.js
@@ -2,6 +2,36 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../ducks';
 
+export function persistState(reducerModule, update) {
+  try {
+    let state = {}
+    const serializedState = localStorage.getItem('persistedState');
+    if (serializedState !== null) {
+      state = JSON.parse(serializedState);
+    }
+    state[reducerModule] = state[reducerModule] || {}
+    Object.assign(state[reducerModule], update)
+    localStorage.setItem('persistedState', JSON.stringify(state));
+  } catch (err) {
+    // empty
+  }
+}
+
+export function getPersistedState(reducerModule) { 
+  let persistedState = {}
+  try {
+    const serializedState = localStorage.getItem('persistedState');
+    if (serializedState !== null) {
+      persistedState = JSON.parse(serializedState);
+      return persistedState[reducerModule] 
+    } 
+  } catch (err) {
+    // empty
+  }
+
+  return {}
+}
+
 const finalCreateStore = compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
