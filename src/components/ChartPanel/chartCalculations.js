97b6699bfa94cb2d3b3e3c776c48fea2030fded5
Clement Balestrat <>
2020-01-22 17:56:18 +1100

    removed useless Math.abs in chartcalculations

--- src/components/ChartPanel/chartCalculations.js
+++ src/components/ChartPanel/chartCalculations.js
@@ -2,7 +2,7 @@ export const calculateRateChange = data => {
 	if (data.length < 2) return 0;
 	const newPrice = data[0].rate;
 	const oldPrice = data[data.length - 1].rate;
-	const percentageChange = (newPrice - oldPrice) / Math.abs(oldPrice);
+	const percentageChange = (newPrice - oldPrice) / oldPrice;
 	return percentageChange;
 };
 
