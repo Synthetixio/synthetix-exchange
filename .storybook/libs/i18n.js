8367d8a6226ee5093dd45592a0a376ac0a9604d4
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-02-24 08:52:47 +1100

    refactor(i18n): inline translations for now (until we support more languages)

--- .storybook/libs/i18n.js
+++ .storybook/libs/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import enTranslation from '../../public/locales/en/translation.json';
+import enTranslation from '../../src/shared/translations/en.json';
 
 i18n.use(initReactI18next).init({
 	resources: {
