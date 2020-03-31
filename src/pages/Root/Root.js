9bdd84fcbe363da1a9102e54f3cb56b0c8dddd93
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-03-31 16:26:25 +1100

    fix: remove dapp maintenance

--- src/pages/Root/Root.js
+++ src/pages/Root/Root.js
@@ -15,7 +15,6 @@ import { setExchangeFeeRate, setNetworkGasInfo } from '../../ducks/transaction';
 
 import { setAppReady, getIsAppReady } from '../../ducks/app';
 
-import MaintenanceMessage from './components/MaintenanceMessage';
 import App from './App';
 
 const REFRESH_INTERVAL = 3 * 60 * 1000;
@@ -33,27 +32,12 @@ const Root = ({
 	isAppReady,
 }) => {
 	const [intervalId, setIntervalId] = useState(null);
-	const [appIsOnMaintenance, setAppIsOnMaintenace] = useState(false);
 	const fetchAndSetExchangeData = useCallback(async synths => {
 		const { exchangeFeeRate, networkPrices, frozenSynths } = await getExchangeData(synths);
 		setExchangeFeeRate(exchangeFeeRate);
 		setNetworkGasInfo(networkPrices);
 		updateFrozenSynths({ frozenSynths });
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	const fetchMaintenanceState = useCallback(async () => {
-		if (process.env.REACT_APP_CONTEXT !== 'production') return;
-		const {
-			snxJS: { DappMaintenance },
-		} = snxJSConnector;
-		try {
-			const isOnMaintenance = await DappMaintenance.isPausedSX();
-			setAppIsOnMaintenace(isOnMaintenance);
-		} catch (e) {
-			console.log(e);
-			setAppIsOnMaintenace(false);
-		}
 	}, []);
 
 	useEffect(() => {
@@ -82,7 +66,6 @@ const Root = ({
 			setAvailableSynths({ synths });
 			setAppReady();
 			fetchAndSetExchangeData(synths);
-			fetchMaintenanceState();
 			clearInterval(intervalId);
 			const _intervalId = setInterval(() => {
 				fetchAndSetExchangeData(synths);
@@ -99,7 +82,7 @@ const Root = ({
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [fetchAndSetExchangeData, fetchWalletBalancesRequest]);
 
-	return appIsOnMaintenance ? <MaintenanceMessage /> : <App isAppReady={isAppReady} />;
+	return <App isAppReady={isAppReady} />;
 };
 
 const mapStateToProps = state => ({
