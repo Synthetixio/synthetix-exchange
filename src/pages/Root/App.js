86028696a09a0f048b4e398c13a8f66004496de5
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-05-04 14:42:45 +1000

    refactor: replace the onboarding chart with an svg

--- src/pages/Root/App.js
+++ src/pages/Root/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import ChartLineSVG from 'src/assets/images/l2/chart-line.svg';
 
 import history from '../../utils/history';
 
@@ -41,58 +42,65 @@ const App = ({
 	walletPopupIsVisible,
 	walletInfo,
 	viewTxModalVisible,
-}) => (
-	<ThemeProvider theme={darkTheme}>
-		{isAppReady ? (
-			<>
-				<GlobalEventsGate />
-				{walletPopupIsVisible && <WalletPopup />}
-				{gweiPopupIsVisible && <GweiPopup />}
-				{leaderboardPopupIsVisible && <LeaderboardPopup />}
-				{twitterPopupIsVisible && <TwitterPopup />}
-				{viewTxModalVisible && <ViewTxModal />}
-				<Router history={history}>
-					<Switch>
-						<Route
-							path={ROUTES.TradeMatch}
-							render={routeProps => (
-								<>
-									{walletInfo.twitterFaucet > 0 ? (
-										<MainLayout isAppReady={isAppReady}>
-											<Trade {...routeProps} />
-										</MainLayout>
-									) : (
-										<Redirect to={ROUTES.Home} />
-									)}
-								</>
-							)}
-						/>
-						<Route
-							path={ROUTES.Trade}
-							render={routeProps => (
-								<>
-									{walletInfo.twitterFaucet > 0 ? (
-										<MainLayout isAppReady={isAppReady}>
-											<Trade {...routeProps} />
-										</MainLayout>
-									) : (
-										<Redirect to={ROUTES.Home} />
-									)}
-								</>
-							)}
-						/>
-						<Route path={ROUTES.Home} component={Onboarding} />
-					</Switch>
-				</Router>
-			</>
-		) : (
-			<MainLayout isAppReady={isAppReady} isOnSplashPage={true}>
-				<div />
-				{/* TODO: handle retries */}
-			</MainLayout>
-		)}
-	</ThemeProvider>
-);
+}) => {
+	// preloading the chart line image to prevent the bounce on the onboarding page. (quickest solution for now)
+	useEffect(() => {
+		new Image().src = ChartLineSVG;
+	}, []);
+
+	return (
+		<ThemeProvider theme={darkTheme}>
+			{isAppReady ? (
+				<>
+					<GlobalEventsGate />
+					{walletPopupIsVisible && <WalletPopup />}
+					{gweiPopupIsVisible && <GweiPopup />}
+					{leaderboardPopupIsVisible && <LeaderboardPopup />}
+					{twitterPopupIsVisible && <TwitterPopup />}
+					{viewTxModalVisible && <ViewTxModal />}
+					<Router history={history}>
+						<Switch>
+							<Route
+								path={ROUTES.TradeMatch}
+								render={routeProps => (
+									<>
+										{walletInfo.twitterFaucet > 0 ? (
+											<MainLayout isAppReady={isAppReady}>
+												<Trade {...routeProps} />
+											</MainLayout>
+										) : (
+											<Redirect to={ROUTES.Home} />
+										)}
+									</>
+								)}
+							/>
+							<Route
+								path={ROUTES.Trade}
+								render={routeProps => (
+									<>
+										{walletInfo.twitterFaucet > 0 ? (
+											<MainLayout isAppReady={isAppReady}>
+												<Trade {...routeProps} />
+											</MainLayout>
+										) : (
+											<Redirect to={ROUTES.Home} />
+										)}
+									</>
+								)}
+							/>
+							<Route path={ROUTES.Home} component={Onboarding} />
+						</Switch>
+					</Router>
+				</>
+			) : (
+				<MainLayout isAppReady={isAppReady} isOnSplashPage={true}>
+					<div />
+					{/* TODO: handle retries */}
+				</MainLayout>
+			)}
+		</ThemeProvider>
+	);
+};
 
 App.propTypes = {
 	isAppReady: PropTypes.bool.isRequired,
