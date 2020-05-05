f24ba67348098fa29f69dff9540fd7366fcc46e9
Clement Balestrat <clement@synthetix.io>
2020-05-06 09:22:18 +1000

    copy update

--- src/components/TwitterPopup/TwitterPopup.js
+++ src/components/TwitterPopup/TwitterPopup.js
@@ -122,8 +122,8 @@ const TwitterPopup = ({ hideTwitterPopup, walletInfo }) => {
 					<Headline>Tweet @ us to get started</Headline>
 					<Subtitle>
 						An L2 testnet trading competition powered by the OVM. Experience the speed of optimistic
-						rollups and compete to win 50k SNX. A new wallet in the Tweet preview below has been
-						generated for you to use in the competition.
+						rollups and compete to win 50k SNX. <br /> A new address in the Tweet preview below has
+						been generated for you to use in the competition. Do not replace this address!
 					</Subtitle>
 					<CloseButton onClick={hideTwitterPopup}>
 						<CloseCrossIcon />
