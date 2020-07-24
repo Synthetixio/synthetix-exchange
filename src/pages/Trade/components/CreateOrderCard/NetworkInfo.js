b7e9e8586ce43864bdf7791b173200300108dc22
dvd-schwrtz <dbschwartz17@gmail.com>
2020-07-24 13:51:39 -0700

    feat(GasFees): change gas fees input mechanism on syntehtix exchange trading interface

--- src/pages/Trade/components/CreateOrderCard/NetworkInfo.js
+++ src/pages/Trade/components/CreateOrderCard/NetworkInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -8,9 +8,14 @@ import { formatCurrency } from 'utils/formatters';
 import { getTransactionPrice } from 'utils/networkUtils';
 
 import { LinkTextSmall } from 'shared/commonStyles';
-import { TextButton, FlexDivRow } from 'shared/commonStyles';
+import { FlexDivRow } from 'shared/commonStyles';
+import { media } from 'shared/media';
 
+import GasMenu from 'pages/shared/components/GasMenu';
+
+import DropdownPanel from 'components/DropdownPanel';
 import { DataSmall } from 'components/Typography';
+
 import { ReactComponent as QuestionMark } from 'assets/images/question-mark.svg';
 import { formatPercentage } from 'utils/formatters';
 
@@ -21,9 +26,17 @@ export const TransactionInfo = ({
 	usdRate = 0,
 	amount = 0,
 	exchangeFeeRate = 0,
-	onEditButtonClick,
 }) => {
 	const { t } = useTranslation();
+
+	const [gasDropdownIsOpen, setGasDropdownIsOpen] = useState(false);
+	const setDropdownIsOpen = (isOpen) => {
+		if (!isOpen && !gasDropdownIsOpen) {
+			return;
+		}
+		setGasDropdownIsOpen(isOpen);
+	};
+
 	const usdValue = amount * usdRate;
 	const exchangeFee = ((amount * exchangeFeeRate) / 100) * usdRate;
 	const networkFee = getTransactionPrice(gasPrice, gasLimit, ethRate);
@@ -63,10 +76,20 @@ export const TransactionInfo = ({
 			<NetworkDataRow>
 				<NetworkData>{t('common.gas-price-gwei')}</NetworkData>
 				<NetworkData>
-					{gasPrice || 0}
-					<ButtonEdit onClick={onEditButtonClick}>
-						<LinkTextSmall>{t('common.actions.edit')}</LinkTextSmall>
-					</ButtonEdit>
+					<StyledDropdownPanel
+						height="auto"
+						isOpen={gasDropdownIsOpen}
+						handleClose={() => setDropdownIsOpen(false)}
+						width="150px"
+						onHeaderClick={() => setDropdownIsOpen(!gasDropdownIsOpen)}
+						header={
+							<GasEditFields>
+								<>{formatCurrency(gasPrice) || 0}</>
+								<LinkTextSmall>{t('common.actions.edit')}</LinkTextSmall>
+							</GasEditFields>
+						}
+						body={<GasMenu setDropdownIsOpen={setDropdownIsOpen} />}
+					/>
 				</NetworkData>
 			</NetworkDataRow>
 		</Container>
@@ -77,8 +100,25 @@ TransactionInfo.propTypes = {
 	gasPrice: PropTypes.number,
 	gasLimit: PropTypes.number,
 	ethRate: PropTypes.number,
-	onEditButtonClick: PropTypes.func.isRequired,
 };
+
+const StyledDropdownPanel = styled(DropdownPanel)`
+	${media.medium`
+		width: auto;
+	`}
+	.body {
+		border-width: 1px 1px 1px 1px;
+		margin-top: 20px;
+	}
+`;
+
+const GasEditFields = styled.div`
+	display: flex;
+	width: 75px;
+	float: right;
+	justify-content: space-between;
+	cursor: pointer;
+`;
 
 const TooltipContent = styled.div`
 	width: 200px;
@@ -132,10 +172,6 @@ const NetworkDataRow = styled(FlexDivRow)`
 	display: flex;
 	align-items: center;
 	margin-bottom: 8px;
-`;
-
-const ButtonEdit = styled(TextButton)`
-	margin-left: 10px;
 `;
 
 export default TransactionInfo;
---
+++ src/pages/shared/components/GasMenu/GasMenu.tsx
@@ -0,0 +1,114 @@
+import React, { FC, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { ConnectedProps, connect } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+
+import { setGasPrice, getGasInfo } from 'ducks/transaction';
+import { RootState } from 'ducks/types';
+
+import NumericInput from 'components/Input/NumericInput';
+
+const mapStateToProps = (state: RootState) => ({
+	gasInfo: getGasInfo(state),
+});
+
+const mapDispatchToProps = {
+	setGasPrice,
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type GasMenuProps = PropsFromRedux & {
+	setDropdownIsOpen: (isOpen: boolean) => void;
+};
+
+const GasMenu: FC<GasMenuProps> = ({ setGasPrice, gasInfo: { gasSpeed }, setDropdownIsOpen }) => {
+	const { t } = useTranslation();
+	const [customGasPrice, setCustomGasPrice] = useState<number | undefined>(undefined);
+
+	const setGasPriceAndCloseDropdown = (gasPrice: number) => {
+		setGasPrice(gasPrice);
+		setDropdownIsOpen(false);
+	};
+
+	useEffect(() => {
+		// eslint-disable-next-line eqeqeq
+		if (customGasPrice != undefined) {
+			setGasPrice(customGasPrice);
+		}
+	}, [setGasPrice, customGasPrice]);
+
+	return (
+		<Content>
+			<StyledNumericInput
+				id="1"
+				value={customGasPrice}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					const newPrice = Number(e.target.value);
+					if (newPrice < 300) {
+						setCustomGasPrice(newPrice);
+					}
+				}}
+				placeholder="CUSTOM"
+				step="0.1"
+				min="0"
+			/>
+			<DefinedGasSelector onClick={() => setGasPriceAndCloseDropdown(gasSpeed.slowAllowed)}>
+				<div>{t('modals.gwei.table.slow')}</div>
+				<div>{gasSpeed.slowAllowed}</div>
+			</DefinedGasSelector>
+			<DefinedGasSelector onClick={() => setGasPriceAndCloseDropdown(gasSpeed.averageAllowed)}>
+				<div>{t('modals.gwei.table.average')}</div>
+				<div>{gasSpeed.averageAllowed}</div>
+			</DefinedGasSelector>
+			<DefinedGasSelector onClick={() => setGasPriceAndCloseDropdown(gasSpeed.fastestAllowed)}>
+				<div>{t('modals.gwei.table.fast')}</div>
+				<div>{gasSpeed.fastestAllowed}</div>
+			</DefinedGasSelector>
+		</Content>
+	);
+};
+
+const Content = styled.div`
+	width: 100%;
+	height: 100%;
+`;
+
+const StyledNumericInput = styled(NumericInput)`
+	width: calc(100% - 20px);
+	margin: 10px 10px 0 10px;
+	font-size: 12px;
+	height: 36px;
+	color: ${({ theme }) => theme.colors.fontTertiary};
+	background-color: ${({ theme }) => theme.colors.accentL1};
+`;
+
+const DefinedGasSelector = styled.div`
+	padding: 10px;
+	margin: 10px;
+	cursor: pointer;
+	display: flex;
+	justify-content: space-between;
+	color: ${({ theme }) => theme.colors.fontTertiary};
+	height: 32px;
+	&:hover {
+		color: ${({ theme }) => theme.colors.fontPrimary};
+		background-color: ${({ theme }) => theme.colors.accentL1};
+		span {
+			background-color: ${(props) => props.theme.colors.accentL2};
+		}
+	}
+	&.active {
+		background-color: ${({ theme }) => theme.colors.accentL2};
+		color: ${({ theme }) => theme.colors.fontPrimary};
+		&:hover {
+			span {
+				background-color: ${(props) => props.theme.colors.accentL1};
+			}
+		}
+	}
+`;
+
+export default connector(GasMenu);
