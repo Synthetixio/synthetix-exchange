d557733708b643741eba45ac34ee036d0ea7fc4b
dvd-schwrtz <dbschwartz17@gmail.com>
2020-07-31 12:16:50 -0700

    feat: improve error handling for custom gas input

--- src/pages/shared/components/SelectGasMenu/SelectGasMenuBody.tsx
+++ src/pages/shared/components/SelectGasMenu/SelectGasMenuBody.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { ConnectedProps, connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -7,6 +7,8 @@ import { setGasPrice, getGasInfo } from 'ducks/transaction';
 import { RootState } from 'ducks/types';
 
 import NumericInput from 'components/Input/NumericInput';
+
+import { Message } from 'shared/commonStyles';
 
 const MAX_GAS_MULTIPLE = 1.5;
 
@@ -33,17 +35,27 @@ const SelectGasMenuBody: FC<GasMenuProps> = ({
 }) => {
 	const { t } = useTranslation();
 	const [customGasPrice, setCustomGasPrice] = useState<number | undefined>(undefined);
+	const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
 	const setGasPriceAndCloseDropdown = (updateGasPrice: number) => {
 		setGasPrice(updateGasPrice);
 		setDropdownIsOpen(false);
 	};
 
+	const gasPriceLimit = useMemo(() => Math.floor(gasSpeed.fastestAllowed * MAX_GAS_MULTIPLE), [
+		gasSpeed.fastestAllowed,
+	]);
+
 	useEffect(() => {
 		if (customGasPrice !== undefined) {
-			setGasPrice(customGasPrice);
+			setGasPrice(customGasPrice > gasPriceLimit ? gasPriceLimit : customGasPrice);
+			setErrorMessage(
+				customGasPrice > gasPriceLimit
+					? t('common.errors.gas-exceeds-limit', { gasPrice: gasPriceLimit })
+					: undefined
+			);
 		}
-	}, [setGasPrice, customGasPrice]);
+	}, [setGasPrice, customGasPrice, gasPriceLimit, t]);
 
 	return (
 		<Content>
@@ -51,15 +63,19 @@ const SelectGasMenuBody: FC<GasMenuProps> = ({
 				value={customGasPrice}
 				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
 					const newPrice = Number(e.target.value);
-					if (newPrice < gasSpeed.fastestAllowed * MAX_GAS_MULTIPLE) {
-						setCustomGasPrice(newPrice);
-					}
+					setCustomGasPrice(newPrice);
 				}}
 				placeholder={t('modals.gwei.placeholder')}
 				step="0.1"
 				min="0"
 			/>
+			{errorMessage && (
+				<StyledMessage type="error" floating={true} size="sm">
+					{errorMessage}
+				</StyledMessage>
+			)}
 			<DefinedGasSelector
+				needsPadding={errorMessage !== undefined}
 				isActive={gasPrice === gasSpeed.slowAllowed}
 				onClick={() => setGasPriceAndCloseDropdown(gasSpeed.slowAllowed)}
 			>
@@ -98,7 +114,7 @@ const StyledNumericInput = styled(NumericInput)`
 	background-color: ${({ theme }) => theme.colors.accentL1};
 `;
 
-const DefinedGasSelector = styled.div<{ isActive: boolean }>`
+const DefinedGasSelector = styled.div<{ isActive: boolean; needsPadding?: boolean }>`
 	padding: 10px;
 	margin: 10px;
 	cursor: pointer;
@@ -106,6 +122,7 @@ const DefinedGasSelector = styled.div<{ isActive: boolean }>`
 	justify-content: space-between;
 	color: ${(props) => props.theme.colors.fontTertiary};
 	${(props) => props.isActive && `background-color: ${props.theme.colors.accentL1}`};
+	${(props) => props.needsPadding && 'margin-top: 37px'};
 	height: 32px;
 	&:hover {
 		color: ${({ theme }) => theme.colors.fontPrimary};
@@ -114,6 +131,13 @@ const DefinedGasSelector = styled.div<{ isActive: boolean }>`
 			background-color: ${(props) => props.theme.colors.accentL2};
 		}
 	}
+`;
+
+const StyledMessage = styled(Message)`
+	width: calc(100% - 20px);
+	border-radius: 4px;
+	margin-top: 6px;
+	margin-left: 10px;
 `;
 
 export default connector(SelectGasMenuBody);
