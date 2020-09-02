2405ee41bcbd86837e803135b2c5cf3412cf7ae6
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-09-02 20:24:48 +1000

    increase gas limit to 10k

--- src/constants/transaction.ts
+++ src/constants/transaction.ts
@@ -6,5 +6,5 @@ export const TRANSACTION_STATUS = {
 	CANCELLED: 'cancelled',
 };
 
-export const GAS_LIMIT_BUFFER = 5000;
+export const GAS_LIMIT_BUFFER = 10000;
 export const DEFAULT_GAS_LIMIT = 500000;
