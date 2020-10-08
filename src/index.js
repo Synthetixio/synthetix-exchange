6ef550d4b502e890a0fa41d34e7505fbdafdbdc1
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-10-08 14:41:05 +1100

    put sX down

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ console.log(bugsnag.releaseStage);
 
 bugsnagClient.use(bugsnagReact, React);
 
-const SYSTEM_ON_MAINTENANCE = false;
+const SYSTEM_ON_MAINTENANCE = true;
 
 const ErrorBoundary = bugsnagClient.getPlugin('react');
 const App = () => {
