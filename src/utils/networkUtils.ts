e41a025b39841ddf4e048557bd09d9c28e507ec4
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-11-18 10:59:24 +1100

    fix: use the correct "type" for coinbase wallet

--- src/utils/networkUtils.ts
+++ src/utils/networkUtils.ts
@@ -44,7 +44,7 @@ export const SUPPORTED_WALLETS_MAP: Record<WalletType, string> = {
 	METAMASK: 'Metamask',
 	TREZOR: 'Trezor',
 	LEDGER: 'Ledger',
-	COINBASE: 'Coinbase Wallet',
+	COINBASE: 'Coinbase',
 	WALLET_CONNECT: 'WalletConnect',
 	PORTIS: 'Portis',
 };
